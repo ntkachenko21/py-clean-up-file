4c43eb5fd94a285fe534cf5a0d87c3a81b5eeb69
Nikita Tkachenko <7heswatgg@gmail.com>
2025-03-04 21:03:41 +0100

    remove errors

--- app/main.py
+++ app/main.py
@@ -1,21 +1,15 @@
-import os
-import types
+from __future__ import annotations
 
-from typing import Self, Type
+import os
 
 
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> Self:
+    def __enter__(self) -> CleanUpFile:
         return self
 
-    def __exit__(
-            self,
-            exc_type: Type[BaseException] | None,
-            exc_val: BaseException | None,
-            exc_tb: types.TracebackType | None
-    ) -> None:
+    def __exit__(self, exc_type: any, exc_val: any, exc_tb: any) -> None:
         if os.path.exists(self.filename):
             os.remove(self.filename)
